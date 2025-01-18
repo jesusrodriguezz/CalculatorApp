baf6b115af3777f85639b339ab8882da80c73d18
jesusrodriguezz <130525663+jesusrodriguezz@users.noreply.github.com>
2025-01-18 01:26:23 -0800

    Added the main c file

---
+++ CalculatorApp.c
@@ -0,0 +1,24 @@
+/*++
+
+Module Name: 
+
+   Calculator App 
+
+Abstract: 
+
+   A advance calculator app that can do basic arithmetic/trigonometric/logarithmic operations 
+   “+”,”-“,”*”,”/”, ”^”, sin, cos, tan, cot, ln, log10 with real numbers involving parenthesis and curly brackets
+
+Author: 
+
+   Jesus Rodriguez
+
+Class:
+
+   Advanced Software Engineering(CS480)
+
+--*/
+#include <stdio.h>
+int main() {
+   printf("hi");
+}
